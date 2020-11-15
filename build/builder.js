0affcf5a691a2252f63068ddd1283984569c5949
Ernesto Borio <petruza@Mac-mini.local>
2020-11-15 19:14:27 -0300

    Improvement.

--- build/builder.js
+++ build/builder.js
@@ -1,4 +1,7 @@
+// TODO: Build JS and Java in two concurrent child processes to take advantage of multiple CPU cores
+
 const { spawn } = require('child_process');
+const colors = require('colors');
 
 const basePath = `${__dirname}/..`;
 const args = [
@@ -8,16 +11,16 @@ const args = [
 ];
 
 module.exports = function (buildName) {
-	console.log(`Started build ${buildName}`);
+	console.log('Build '.blue + buildName + ' started'.blue);
 	this.proc = spawn(`${__dirname}/build.sh`, args, { cwd: __dirname });
 
 	this.proc.on('error', err => {
-		console.error(`    Error on build ${buildName}: ${err}`);
+		console.error(`Error on build ${buildName}: ${err}\n`.brightRed);
 		this.proc.kill('SIGUSR1'); // means the build had an error
 	});
 
 	this.proc.stderr.on('data', chunk => {
-		console.error(`    Error on build ${buildName}: ${chunk.toString()}`);
+		console.error(`Error on build ${buildName}: ${chunk.toString()}\n`.brightRed);
 		this.proc.kill('SIGUSR1'); // means the build had an error
 	});
 
@@ -25,10 +28,10 @@ module.exports = function (buildName) {
 		this.proc = null;
 		if (signal !== 'SIGUSR1') {
 			if (code === 0 && signal === null) {
-				console.log(`    Completed build ${buildName}`);
+				console.log('Build '.green + buildName + ' completed\n'.green);
 			}
 			else {
-				console.log(`    Stopped build ${buildName}`);
+				console.log('Build '.gray + buildName + ' cancelled\n'.gray);
 			}
 		}
 		if (this.callback) {
--- build/watch.js
+++ build/watch.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const colors = require('colors');
 const Build = require('./builder');
 
 // Build numbers to distinguish each build run
 let buildNum = 1;
 
-let currentBuild = new Build(buildNum);
+let currentBuild = new Build(rotateColor(buildNum));
 
 const rebuild = () => {
 	buildNum++;
-	currentBuild = new Build(buildNum);
+	currentBuild = new Build(rotateColor(buildNum));
 }
 
+// VSCode fires two consecutive change events when hitting save, and we can't efficiently tell them apart, so whatever.
 fs.watch(`${__dirname}/../src/wikitext.g4`, event => {
 	if (event === 'change') {
 		if (currentBuild.proc) {
@@ -22,3 +24,13 @@ fs.watch(`${__dirname}/../src/wikitext.g4`, event => {
 		}
 	}
 });
+
+function rotateColor(num) {
+	let col = num.toString();
+	switch (num % 2) {
+		case 0:
+			return col.yellow;
+		default:
+			return col.magenta;
+	}
+}
